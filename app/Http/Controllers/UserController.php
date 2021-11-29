30aaf908ab589f1371347806e25dd936b3f71ab9
Prabawas <afdoniprabawas319@gmail.com>
2021-11-29 13:53:17 +0700

    updet user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,9 +11,9 @@ class UserController extends Controller
     public function profil()
     {
         return view('user.profil', [
-        'title' => 'profil pengurus',
-        'users' => User::all()
-    ]);
+            'title' => 'profil pengurus',
+            'users' => User::all()
+        ]);
     }
 
     public function register()
@@ -37,37 +37,35 @@ class UserController extends Controller
             'password' => 'required | min:6 | max:8',
         ]);
 
-         if($request->hasFile('avatar')) {
+        if ($request->hasFile('avatar')) {
             // Mengambil gambar dan menyimpannya di folder tujuan dengan nama asli
             $request->file('avatar')->move('image/', $request->file('avatar')->getClientOriginalName());
             $regist['avatar'] = $request->file('avatar')->getClientOriginalName();
-           
         }
 
-         $regist['password'] = bcrypt($regist['password']);    //Meng-enkripsi password
+        $regist['password'] = bcrypt($regist['password']);    //Meng-enkripsi password
 
-          User::create($regist);
-          return redirect('/profil')->with('success', ' Petugas berhasil ditambah!');
-        
+        User::create($regist);
+        return redirect('/profil')->with('success', ' Petugas berhasil ditambah!');
     }
 
-     public function login()
+    public function login()
     {
         return view('user.login', [
             'title' => 'Halaman Login'
         ]);
     }
 
-    public function loginAkun(Request $request) 
+    public function loginAkun(Request $request)
     {
-         $auth = $request->validate([
+        $auth = $request->validate([
             'email' => 'required | email',
             'password' => 'required | max:8',
         ]);
 
-        if(Auth::attempt($auth)) {  //Jika data yang diinput sesuai
+        if (Auth::attempt($auth)) {  //Jika data yang diinput sesuai
             $request->session()->regenerate();
-            
+
             return redirect()->intended('/'); //diarahkan ke halaman dashboard
         }
 
@@ -75,36 +73,38 @@ class UserController extends Controller
     }
 
     public function logoutAkun(Request $request) //untuk logout
-   {
+    {
         Auth::logout();
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
         return redirect('/');   //diarahkan ke halaman utama
-   }
+    }
 
-   public function detail($id)
-   {
-         $users = User::find($id);
+    public function detail($id)
+    {
+        $users = User::find($id);
 
         return view('user.detail', [
             'user' => $users,
-            'title' => 'detail profil']);
-   }
+            'title' => 'detail profil'
+        ]);
+    }
 
-  public function edit($id) 
-  {
+    public function edit($id)
+    {
         // Mengambil data dari database berdasarkan id yang dipilih lalu membuka halaman update
         $users = User::find($id);
 
         return view('user.update', [
             'users' => $users,
-            'title' => 'edit profil']);
+            'title' => 'edit profil'
+        ]);
     }
 
-   public function update(Request $request, User $user)
-   {
+    public function update(Request $request, User $user)
+    {
         // $user = User::find($id);
         // $user->update($request->all());
         // if($request->hasFile('avatar')) {
@@ -124,21 +124,21 @@ class UserController extends Controller
             'email' => 'required | email',
         ]);
 
-         if($request->hasFile('avatar')) {
+        if ($request->hasFile('avatar')) {
             // Mengambil gambar dan menyimpannya di folder tujuan dengan nama asli
             $request->file('avatar')->move('image/', $request->file('avatar')->getClientOriginalName());
             $regist['avatar'] = $request->file('avatar')->getClientOriginalName();
-           
         }
 
-         User::where('id', $user->id)
-                ->update($regist);
+        User::where('id', $user->id)
+            ->update($regist);
 
 
-        return redirect('/profil')->with('edit', ' Profil berhasil diedit!');
-   }
+        return redirect('/profil')->with('edit', ' Profil telah berhasil diedit!');
+    }
 
-    public function delete($id) {
+    public function delete($id)
+    {
         // Memilih data dari database berdasarkan id dan menghapusnya dengan fungsi delete()
         // lalu kembali ke halaman data user
         $users = User::find($id);
@@ -146,5 +146,4 @@ class UserController extends Controller
 
         return redirect('/profil')->with('hapus', ' Profil berhasil dihapus!');
     }
-    
 }
